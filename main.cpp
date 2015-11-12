5eeebe51f99d98a131fdd91c7878e51f61cc28c6
Michael Munson <michaelmunson1@gmail.com>
2015-11-12 15:41:37 -0500

    Add strings, vectors, timing code

--- main.cpp
+++ main.cpp
@@ -9,46 +9,73 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-int* naiveMatch(const char* P, const char* T){
-    int* matches = new int[3];
-    int k = 0;
+vector<int> naiveMatch(string P_mult, string T_mult){
+    vector<int> matches;
     
-    for (int i=0; i <= strlen(T) - strlen(P); ++i)
+    for (int i=0; i <= T_mult.length() - P_mult.length(); ++i)
     {
-        for(int j=0; j < strlen(P); ++j)
+        for(int j=0; j < P_mult.length(); ++j)
         {
-            if (P[j] != T[i+j]) break;
-            else if (j == strlen(P) - 1)
-            {
-                matches[k] = i;
-                ++k;
-            }
+            if (P_mult[j] != T_mult[i+j]) break;
+            else if (j == P_mult.length() - 1) matches.push_back(i);
         }
     }
     return matches;
 }
 
+
 int main(int argc, const char * argv[])
 {
-    // insert code here...
-    cout << "Hello, World!\n";
     
-    const char* P = "aba";
-    const char* T = "bbabaxababay";
     
-    //int diff = strlen(T) - strlen(P) + 1;
+    const string P = "aba";
+    const string T = "bbabaxababay";
     
-    int *matches = naiveMatch(P,T);
+    string P_mult = "";      // for time tests of input size
+    string T_mult = "";
     
-    for (int i=0; i < 3; ++i){
-        cout << matches[i] << endl;
+    clock_t startTime;
+    
+    vector<double> times(4);
+    
+    double minTime;
+    double thisTime;
+    
+    vector<int> matches;
+    
+    
+    for(int i=0; i < 4; ++i){
+        
+        //cout << "i is " << i << endl;
+        int inSize = (int) pow(10.0, (double) i);
+        
+        for (int k=0; k< inSize; ++k){
+            P_mult += P;
+            T_mult += T;
+        }
+        
+        minTime = 9999999.9;
+        for (int j=0; j < 5; ++j){
+            //cout << "j is " << j << endl;
+            
+            startTime = clock();
+            //cout << startTime << endl;
+            matches = naiveMatch(P_mult,T_mult);
+            //cout << clock() << endl;
+            thisTime = double( clock() - startTime );
+            if (thisTime < minTime)  minTime = thisTime;
+        }
+        times[i] = minTime;
+        
     }
     
-    delete[] matches;
+    for(int i = 0; i < 4; ++i){
+        cout << times[i] << endl;
+    }
+    
     return 0;
 }
-
-
