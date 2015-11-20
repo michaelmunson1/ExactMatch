cc1848e1a135dd6c366be9b61d8ab9eb86542831
Munson <michaelmunson1@gmail.com>
2015-11-20 16:21:49 -0500

    completes implementation of simplest linear time string matching algorithm (based on fundamental string pre-processing)

--- ExactMatch.h
+++ ExactMatch.h
@@ -18,7 +18,9 @@ vector<int> naiveMatch(string, string);
 vector<int> timeMatchingAlgo(vector<int> (*algo)(string,string), string, string);
 vector<int> simplestLinearMatch(string, string);
 vector<int> zPre(string P);
-vector<int> findZMatches(string, int, int, vector<int>);
 void printTimes(vector<int>, int);
+void zPreProcessPatternBody(string, vector<int>&, int, int);
+vector<int> zProcessBody(bool, string, vector<int>& , int, int, int, int);
+vector<int> findZMatchesInTextBody(string, vector<int>&, int, int);
 
 #endif /* ExactMatch_h */
